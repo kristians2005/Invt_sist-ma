2bce5f876f6924bf9fdb5db20ac4497b7c9bf1ff
MarkussIPA22 <markusspetrovs0@gmail.com>
2025-03-12 13:40:28 +0200

    createuser

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -18,13 +18,72 @@ class UsersController
 
     public function create()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $roles = $_POST['roles'];
 
-        require "views/auth/create.view.php";
+            Users::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'roles' => $roles
+            ]);
+
+            header("Location: /users");
+        }
+        require "views/users/Create.view.php";
     }
 
     public function store()
     {
-      
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $password_confirmation = $_POST['password_confirmation'];
+        $roles = $_POST['roles'];
+
+        $error = [];
+
+        if (Validator::required($name)) {
+            $error["name"] = "Name is required.";
+        }
+
+        if (!Validator::strLengt($name, 3, 50)) {
+            $error["name"] = "Name must be between 3 and 50 characters long.";
+        }
+
+        if (!Validator::passwordMatch($password, $password_confirmation)) {
+            $error["password"] = "Passwords do not match.";
+        }
+
+        if (!Validator::passwordContains($password)) {
+            $error["password"] = "Password must contain at least one number and one uppercase letter and one simbol.";
+        }
+
+        if (!Validator::passwordLength($password)) {
+            $error["password"] = "Password must be at least 8 characters long.";
+        }
+
+        if (!Validator::email($email)) {
+            $error["email"] = "Email is not valid.";
+        }
+
+        if (Auth::emailExists($email)) {
+            $error["email"] = "This email is already registered. Please use a different email.";
+        }
+
+
+
+        var_dump($error);
+
+        if (empty($error)) {
+            Auth::register($name, $email, $password);
+            header('Location: /users');
+        } else {
+            require "views/users/Create.view.php";
+        }
     }
 
     public function edit()
