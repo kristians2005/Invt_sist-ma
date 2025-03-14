88893beedb5a497ed9931365bed1226e9d15966f
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2025-03-14 09:43:22 +0200

    buttons

--- views/orders/order.view.php
+++ views/orders/order.view.php
@@ -1,46 +1,51 @@
-<!-- views/orders/order.view.php -->
-<h1>Pasūtīt produktus</h1>
+<?php require_once "views/partials/header.view.php"; ?>
+<div class="p-20">
 
-<?php if (isset($data['success'])): ?>
-    <p style="color: green;"><?php echo $data['success']; ?></p>
-<?php endif; ?>
-<?php if (isset($data['error'])): ?>
-    <p style="color: red;"><?php echo $data['error']; ?></p>
-<?php endif; ?>
+    <h1>Pasūtīt produktus</h1>
 
-<table>
-    <tr>
-        <th>Nosaukums</th>
-        <th>SKU</th>
-        <th>Apraksts</th>
-        <th>Cena</th>
-        <th>Kategorija</th>
-        <th>Atlikums</th>
-        <th>Atrašanās vieta</th>
-        <th>Pasūtīt</th>
-        <th>Detaļas</th>
-    </tr>
-    <?php foreach ($data['products'] as $product): ?>
+    <?php if (isset($data['success'])): ?>
+        <p style="color: green;"><?php echo $data['success']; ?></p>
+    <?php endif; ?>
+    <?php if (isset($data['error'])): ?>
+        <p style="color: red;"><?php echo $data['error']; ?></p>
+    <?php endif; ?>
+
+    <table>
         <tr>
-            <td><?php echo $product['name']; ?></td>
-            <td><?php echo $product['sku']; ?></td>
-            <td><?php echo $product['description'] ?: 'Nav apraksta'; ?></td>
-            <td><?php echo $product['price']; ?> EUR</td>
-            <td><?php echo $product['category']; ?></td>
-            <td><?php echo $product['quantity']; ?></td>
-            <td><?php echo $product['location'] ?: 'Nav norādīts'; ?></td>
-            <td>
-                <form method="POST" action="/orders/order">
-                    <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
-                    <input type="number" name="quantity" min="1" max="<?php echo $product['quantity']; ?>" required style="width: 60px;">
-                    <button type="submit">Pasūtīt</button>
-                </form>
-            </td>
-            <td>
-                <a href="/orders?action=show&id=<?php echo $product['id']; ?>">Skatīt</a>
-            </td>
+            <th>Nosaukums</th>
+            <th>SKU</th>
+            <th>Apraksts</th>
+            <th>Cena</th>
+            <th>Kategorija</th>
+            <th>Atlikums</th>
+            <th>Atrašanās vieta</th>
+            <th>Pasūtīt</th>
+            <th>Detaļas</th>
         </tr>
-    <?php endforeach; ?>
-</table>
+        <?php foreach ($data['products'] as $product): ?>
+            <tr>
+                <td><?php echo $product['name']; ?></td>
+                <td><?php echo $product['sku']; ?></td>
+                <td><?php echo $product['description'] ?: 'Nav apraksta'; ?></td>
+                <td><?php echo $product['price']; ?> EUR</td>
+                <td><?php echo $product['category']; ?></td>
+                <td><?php echo $product['quantity']; ?></td>
+                <td><?php echo $product['location'] ?: 'Nav norādīts'; ?></td>
+                <td>
+                    <form method="POST" action="/orders/order">
+                        <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                        <input type="number" name="quantity" min="1" max="<?php echo $product['quantity']; ?>" required
+                            style="width: 60px;">
+                        <button type="submit">Pasūtīt</button>
+                    </form>
+                </td>
+                <td>
+                    <a href="/orders?action=show&id=<?php echo $product['id']; ?>">Skatīt</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 
-<p><a href="/orders">Atpakaļ uz pārskatu</a></p>
+    <p><a href="/orders">Atpakaļ uz pārskatu</a></p>
+</div>
+<?php require_once "views/partials/footer.view.php"; ?>
